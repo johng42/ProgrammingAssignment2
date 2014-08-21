0278c989761049ffeb732627f40191fe2b65c960
John Guin <johnguin@hotmail.com>
2014-08-21 10:54:18 -0700

    submit correct single file for grading

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function takes a matrix and caches its solved value so that it does
+## not need to be recomputed if requested a second time
 
-makeCacheMatrix <- function(x = matrix()) 
+makeCacheMatrix<-function(m=matrix())
 {
-
+    n<-NULL
+    set <- function(y)
+    {
+        m<<- y
+        n<<- NULL
+    }
+    get<-function() m
+    #need to use the solve() function to compute the inverse
+    setInverse <-function(solve) n<<-solve
+    getInverse <- function() n
+    #return a list of the matrices
+    list(set=set,get=get,
+         setInverse=setInverse,
+         getInverse=getInverse)
 }
 
+## checks to see if the solved data is cached, and returns that value w/o computation if so
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) 
+cacheSolve <-function(m,...)
 {
-        ## Return a matrix that is the inverse of 'x'
+    #quick check to see if the results of solve are cached
+    n<-m$getInverse()
+    if(!is.null(n))
+    {
+        #results are in the cache, so just return the results w/o computation
+        message("Getting cached data...")
+        return (n)
+    }
+    #results were not in cache so need to compute
+    data<-m$get()
+    n<-solve(data,...)
+    #return the inverse
+    m$setInverse(n)
 }
